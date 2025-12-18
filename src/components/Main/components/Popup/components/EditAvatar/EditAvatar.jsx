135a1fc3833ed66439db2e70cdf3bcf4b1a45f93
BiancaBaccin <bianca.baccin@gmail.com>
2025-12-18 20:38:29 -0300

    Ajusta Edit Avatar e New Cards Popup Css

--- src/components/Main/components/Popup/components/EditAvatar/EditAvatar.jsx
+++ src/components/Main/components/Popup/components/EditAvatar/EditAvatar.jsx
@@ -1,21 +1,19 @@
 export default function EditAvatar() {
   return (
-    <form className="photo-profile-popup__form" noValidate>
-      <p className="photo-profile-popup__title">Alterar a foto do perfil</p>
-
-      <fieldset className="photo-profile-popup__fieldset">
+    <form className="popup__form" noValidate>
+      <fieldset className="popup__fieldset">
         <input
-          className="photo-profile-popup__input"
+          className="popup__input"
           type="url"
           placeholder="URL da imagem"
           id="photo-url"
           name="link"
           required
         />
-        <span className="photo-profile-popup__input-error photo-url-error"></span>
+        <span className="popup__input-error photo-url-error"></span>
       </fieldset>
 
-      <button className="photo-profile-popup__submit-button" type="submit">
+      <button className="popup__submit-button" type="submit">
         Salvar
       </button>
     </form>
